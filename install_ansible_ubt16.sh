2586d300327059651f91329a63cb5af6c7cd15f2
Sakura-KAGURA <49386946+Sakura-KAGURA@users.noreply.github.com>
2019-09-27 18:41:51 +0900

    Update install_ansible_ubt16.sh

--- install_ansible_ubt16.sh
+++ install_ansible_ubt16.sh
@@ -1,3 +1,6 @@
+###
+### curl -sf https://raw.githubusercontent.com/Sakura-KAGURA/ubt_setup_script/master/install_ansible_ubt16.sh | sh -s
+
 sudo apt-get update
 sudo apt-get install software-properties-common
 sudo apt-add-repository ppa:ansible/ansible
