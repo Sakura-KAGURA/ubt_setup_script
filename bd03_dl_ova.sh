d61af999a11975fecb2a140243c24d43546f6d55
Sakura-KAGURA <49386946+Sakura-KAGURA@users.noreply.github.com>
2019-10-04 15:20:55 +0900

    Update bd03_dl_ova.sh

--- bd03_dl_ova.sh
+++ bd03_dl_ova.sh
@@ -1,4 +1,6 @@
 
 wget https://images.offensive-security.com/virtual-images/kali-linux-2019.1-vbox-amd64.ova 
+mv kali-linux-2019.1-vbox-amd64.ova ~/
 
 wget https://az792536.vo.msecnd.net/vms/VMBuild_20190311/VirtualBox/MSEdge/MSEdge.Win10.VirtualBox.zip
+mv MSEdge.Win10.VirtualBox.zip ~/
