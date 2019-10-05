edca131d94432c22baa0cc97ab7f747128219790
Sakura-KAGURA <49386946+Sakura-KAGURA@users.noreply.github.com>
2019-10-05 13:51:53 +0900

    Update bd05_k8s-hardway.sh

--- bd05_k8s-hardway.sh
+++ bd05_k8s-hardway.sh
@@ -1,5 +1,5 @@
 
-cd ./Downloads
+cd ~/Downloads
 git clone https://github.com/kenfdev/kubernetes-the-hard-way-vagrant.git
 
-echo "usage : cd ./kubernetes-the-hard-way-vagrant;vagrant up;vagrant status"
+echo "usage : cd ~/Downloads/kubernetes-the-hard-way-vagrant;vagrant up;vagrant status"
