e1dbf5a0288af94dd9a72ac9ddde66d9ac785b67
Sakura-KAGURA <49386946+Sakura-KAGURA@users.noreply.github.com>
2019-10-05 14:25:58 +0900

    Create bd06_k8s_pracenv.sh

---
+++ bd06_k8s_pracenv.sh
@@ -0,0 +1,9 @@
+
+sudo apt install docker-compose
+docker-compose --version
+
+
+# https://github.com/arush-sal/cka-practice-environment
+cd ~/Downloads
+git clone https://github.com/arush-sal/cka-practice-environment.git
+
