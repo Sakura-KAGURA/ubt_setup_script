2c97a9ac3aa1896d2c0e3030fdca649c035888f7
Sakura-KAGURA <49386946+Sakura-KAGURA@users.noreply.github.com>
2019-10-05 13:45:59 +0900

    Create bd99_clearsetup.sh

---
+++ bd99_clearsetup.sh
@@ -0,0 +1,5 @@
+cd ./Downloads
+rm -rf ./ubt_setup_script
+git clone https://github.com/Sakura-KAGURA/ubt_setup_script.git
+chmod +x ./ubt_setup_script/*sh
+cp -p ./ubt_setup_script/bd99_clearsetup.sh ..
