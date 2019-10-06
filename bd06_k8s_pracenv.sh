9953f68ef14d3adb80080aec3eaf8d9b57a38870
Sakura-KAGURA <49386946+Sakura-KAGURA@users.noreply.github.com>
2019-10-06 16:11:09 +0900

    Update bd06_k8s_pracenv.sh

--- bd06_k8s_pracenv.sh
+++ bd06_k8s_pracenv.sh
@@ -1,8 +1,18 @@
 
-sudo apt install docker-compose
+# install docker compose
+#sudo apt install docker-compose
+
+# release
+# https://github.com/docker/compose/releases
+curl -L "https://github.com/docker/compose/releases/download/1.25.0-rc2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+# exit
+#docker-compose -v
+#docker-compose versio
 docker-compose --version
 
 
+# install practice env
 # https://github.com/arush-sal/cka-practice-environment
 cd ~/Downloads
 git clone https://github.com/arush-sal/cka-practice-environment.git
