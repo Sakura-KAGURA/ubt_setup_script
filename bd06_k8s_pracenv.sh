5f19e3da093aba9bec3f388b658c3c3f5d42ed83
Sakura-KAGURA <49386946+Sakura-KAGURA@users.noreply.github.com>
2019-10-06 16:15:33 +0900

    Update bd06_k8s_pracenv.sh

--- bd06_k8s_pracenv.sh
+++ bd06_k8s_pracenv.sh
@@ -4,8 +4,12 @@
 
 # release
 # https://github.com/docker/compose/releases
-curl -L "https://github.com/docker/compose/releases/download/1.25.0-rc2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+## curl -L "https://github.com/docker/compose/releases/download/1.25.0-rc2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+cd ~/Downloads
+curl -L "https://github.com/docker/compose/releases/download/1.25.0-rc2/docker-compose-$(uname -s)-$(uname -m)" 
+sudo  cp -p ./docker-compose-$(uname -s)-$(uname -m)"  /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
 # exit
 #docker-compose -v
 #docker-compose versio
