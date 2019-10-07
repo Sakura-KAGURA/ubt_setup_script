3f7aea52941dbdcc8a2d6cc4790a71b387b8a5fc
Sakura-KAGURA <49386946+Sakura-KAGURA@users.noreply.github.com>
2019-10-07 15:08:45 +0900

    Update kind00_install.sh

--- kind00_install.sh
+++ kind00_install.sh
@@ -1,5 +1,21 @@
 
+# install golang
+
+# GoLANG site
+#https://golang.org/dl/
+#wget https://dl.google.com/go/go1.11.5.linux-amd64.tar.gz
+wget https://dl.google.com/go/go1.13.1.linux-amd64.tar.gz
+
+sudo tar -C /usr/local -xzf go1.13.1.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+
+go version
+
+
 # https://blog.cybozu.io/entry/2019/07/03/170000
 
 GO111MODULE="on" go get sigs.k8s.io/kind@v0.4.0
+
+
+
 
