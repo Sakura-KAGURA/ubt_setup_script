ee83ae76ddafa01f89e40ee7f06a8a3766254512
Sakura-KAGURA <49386946+Sakura-KAGURA@users.noreply.github.com>
2019-10-07 11:36:20 +0900

    Update bd04_dl_vagrant.sh

--- bd04_dl_vagrant.sh
+++ bd04_dl_vagrant.sh
@@ -7,3 +7,6 @@ wget https://releases.hashicorp.com/vagrant/2.2.5/vagrant_2.2.5_x86_64.deb
 sudo apt install ./vagrant_2.2.5_x86_64.deb
 
 which vagrant
+
+# plugin for k8s hardway using vagrant
+vagrant plugin install vagrant-scp
