7a79399a185c094f689e565ce37b82458714d64a
Sakura-KAGURA <49386946+Sakura-KAGURA@users.noreply.github.com>
2019-10-07 16:00:53 +0900

    Update kind01_setup-cluster.sh

--- kind01_setup-cluster.sh
+++ kind01_setup-cluster.sh
@@ -1,3 +1,11 @@
 
+
+
+# usage
+# Cluster creation complete. You can now use the cluster with:
+# export KUBECONFIG="$(kind get kubeconfig-path --name="kind")"
+
+# after set up KUBECONFIG
 # https://blog.cybozu.io/entry/2019/07/03/170000
 kind create cluster --name smplclst
+
