fb0277d7ab0c184536f5cfcbf1ebe2b0debfc7e1
Sakura-KAGURA <49386946+Sakura-KAGURA@users.noreply.github.com>
2019-10-07 10:50:29 +0900

    Create k8shw_02.sh

---
+++ k8shw_02.sh
@@ -0,0 +1,18 @@
+
+## guide
+# https://qiita.com/str416yb/items/01bb1ba2a1b3fd75506f
+
+## install
+# https://gist.github.com/guoyoujin/376bda5323b1d718d8d582f2efa5a8e6
+
+#install:
+#
+#    $ curl -s -L -o /bin/cfssl https://pkg.cfssl.org/R1.2/cfssl_linux-amd64
+#    $ curl -s -L -o /bin/cfssljson https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64
+#    $ curl -s -L -o /bin/cfssl-certinfo https://pkg.cfssl.org/R1.2/cfssl-certinfo_linux-amd64
+#    $ chmod +x /bin/cfssl*
+
+cd ~/Downloads
+curl -O  https://pkg.cfssl.org/R1.2/cfssl_linux-amd64 
+curl -O  https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64
+curl -O  /bin/cfssl-certinfo https://pkg.cfssl.org/R1.2/cfssl-certinfo_linux-amd64
