6921f2dfce49842b98712904aa2da4d9c183eb39
Sakura-KAGURA <49386946+Sakura-KAGURA@users.noreply.github.com>
2019-10-08 16:59:50 +0900

    Update k8s-kubectl-install.sh

--- k8s-kubectl-install.sh
+++ k8s-kubectl-install.sh
@@ -3,16 +3,17 @@
 #
 kubectl version
 KUBEPATH=`which kubectl`
-#KUBE_V1="1.12"
+KUBE_V1="1.12"
 #KUBE_V2="1.16"
 
 
-sudo mv $KUBEPATH ${KUBEPATH}.${KUBE_V1}
 ls -la ${KUBEPATH}*
 #ln -s 
 cd ~/Documents/
 curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.16.0/bin/linux/amd64/kubectl
 chmod +x ./kubectl
+
+sudo mv $KUBEPATH ${KUBEPATH}.${KUBE_V1}
 sudo mv ./kubectl ${KUBEPATH}
 #sudo ln -s ${KUBEPATH}.${KUBE_V2} ${KUBEPATH}
 
