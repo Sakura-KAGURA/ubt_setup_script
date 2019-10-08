0d67d2efc92b96ecc7c7a9407214c83d581d2b99
Sakura-KAGURA <49386946+Sakura-KAGURA@users.noreply.github.com>
2019-10-08 17:47:32 +0900

    Update bd99_clearsetup.sh

--- bd99_clearsetup.sh
+++ bd99_clearsetup.sh
@@ -3,12 +3,14 @@ mv ./ubt_setup_script ./ubt_setup_script.old
 
 #git clone https://github.com/Sakura-KAGURA/ubt_setup_script.git
 # wget https://github.com/Sakura-KAGURA/ubt_setup_script/archive/master.tar.gz
-wget --no-check-certificate https://github.com/Sakura-KAGURA/ubt_setup_script/archive/master.tar.gz
-tar xzvf master.tar.gz
+# wget --no-check-certificate https://github.com/Sakura-KAGURA/ubt_setup_script/archive/master.tar.gz
+wget --no-check-certificate https://github.com/Sakura-KAGURA/ubt_setup_script/archive/master.tar.gz -O ubt_setup_script-master.tar.gz
+
+tar xzvf ubt_setup_script-master.tar.gz
 mv ./ubt_setup_script-master ./ubt_setup_script
 chmod +x ./ubt_setup_script/*sh
 
 cp -p ~/Downloads/ubt_setup_script/bd99_clearsetup.sh ~/Downloads/bd99_clearsetup.sh
 
-rm master.tar.gz
+rm  ubt_setup_script-master.tar.gz
 ls -laR  ~/Downloads/ubt_setup_script/
