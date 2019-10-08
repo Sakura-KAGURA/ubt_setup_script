c1d4f570851b8f0c7e849cb43f53fedfc06b14f6
Sakura-KAGURA <49386946+Sakura-KAGURA@users.noreply.github.com>
2019-10-08 15:50:11 +0900

    Update k8s-kubectl-install.sh

--- k8s-kubectl-install.sh
+++ k8s-kubectl-install.sh
@@ -1,7 +1,12 @@
 
 
 #
+kubectl version
 sudo mv /usr/bin/kubectl /usr/bin/kubectl.1.12
-ln -s 
+#ln -s 
+cd ~/Documents/
+curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.16.0/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/bin/kubectl 
 
 # unlink 
