ae068c9cf8c2504e29aee678110c875a77a7b61e
Sakura-KAGURA <49386946+Sakura-KAGURA@users.noreply.github.com>
2019-10-24 13:59:37 +0900

    Update docker-compose-inst.sh

--- docker-compose-inst.sh
+++ docker-compose-inst.sh
@@ -4,10 +4,15 @@
 #sudo usermod -aG docker $USER
 #sudo systemctl start docker
 #sudo systemctl enable docker
-sudo curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+#sudo curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+#sudo curl -L https://github.com/docker/compose/releases/download/1.6.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+wget https://github.com/docker/compose/releases/download/1.6.2/docker-compose-`uname -s`-`uname -m` 
+sudo mv ./docker-compose* /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 git clone https://github.com/arush-sal/cka-practice-environment.git
+docker-compose --version
+
 cd cka*
 docker-compose up -d 
 
