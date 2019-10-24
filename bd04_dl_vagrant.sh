d30efdf067ea067cbfc93234b9d8e49fb7acd76e
Sakura-KAGURA <49386946+Sakura-KAGURA@users.noreply.github.com>
2019-10-24 10:12:44 +0900

    Update bd04_dl_vagrant.sh

--- bd04_dl_vagrant.sh
+++ bd04_dl_vagrant.sh
@@ -10,3 +10,16 @@ which vagrant
 
 # plugin for k8s hardway using vagrant
 vagrant plugin install vagrant-scp
+
+#gdebiコマンド
+#http://mickey-happygolucky.hatenablog.com/entry/2017/08/05/062959
+
+# https://tech.sairilab.com/2018/09/post-171/
+/etc/apt/sources.list
+sudo echo "deb http://archive.ubuntu.com/ubuntu bionic main universe" >> /etc/apt/sources.list
+echo "deb http://archive.ubuntu.com/ubuntu bionic-security main universe"
+echo "deb http://archive.ubuntu.com/ubuntu bionic-updates main universe"
+
+sudo apt install gdebi
+
+wget https://github.com/absalomedia/vagrant-manager/releases/download/2.3.2/vagrant-manager-amd64.deb
